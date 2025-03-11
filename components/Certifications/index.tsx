399d9d794e45ad1b9d653378c84781bbe90e3940
waqas-khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-03-12 02:26:34 +0500

    site completed

--- components/Certifications/index.tsx
+++ components/Certifications/index.tsx
@@ -1,210 +1,133 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
+import Image from "next/image";
+import Link from "next/link";
 
 const Certifications = () => {
+  return (
+    // Apply overlay and some Hover Effect to this section make this more Readable
+    <div className="mt-10 px-5 md:px-10">
+    
+      <h1 className="text-white text-5xl font-extrabold mb-8 border-b-4 border-lime-400 pb-2 w-max">
+      My Certifications
+      </h1>
 
-    return (
+      <div className="my-10 p-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {certifications.map((cert, index) => (
+            <div
+              key={index}
+              className="relative group overflow-hidden rounded-lg shadow-lg border-b-4 border-lime-400"
+            >
+              <Image
+                className="w-full h-full object-fit rounded-md"
+                src={cert.image}
+                width={500}
+                height={400}
+                alt={cert.title}
+              />
 
-        <div className="mt-10">
-
-            <h1 className="text-white text-5xl text-left font-bold mb-5">My Certifications</h1>
-
-            <div className="my-10 p-3">
-
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/net-fun.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://codedamn.com/certificate/verify/b8be266e289d2659511369640aa385954280c342" >
-                            <h3 className="text-lg font-bold">Web - Internet fundamentals</h3>
-                            <p className="text-gray-600">codedamn</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/wdf.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://www.sololearn.com/Certificate/CT-UARFCW4T/pdf" >
-                            <h3 className="text-lg font-bold">WEB DEVELOPMENT FUNDAMENTALS</h3>
-                            <p className="text-gray-600">Sololearn</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/rwd.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://www.freecodecamp.org/certification/isfhan/responsive-web-design" >
-                            <h3 className="text-lg font-bold">Responsive Web Design</h3>
-                            <p className="text-gray-600">FreeCodeCamp.org</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/bda.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://www.freecodecamp.org/certification/isfhan/back-end-development-and-apis" >
-                            <h3 className="text-lg font-bold">Back End Development and APIs</h3>
-                            <p className="text-gray-600">FreeCodeCamp.org</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/cwa.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://www.udemy.com/certificate/UC-b8064000-0225-4f4f-bf6c-4bde61a14bdc/" >
-                            <h3 className="text-lg font-bold">NodeJs E-Commerce Web API</h3>
-                            <p className="text-gray-600">Udemy</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/mongo-db.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://learn.mongodb.com/c/xwdIcLBfQMm0yOXOSIQhXg" >
-                            <h3 className="text-lg font-bold">MongoDB Node.js Developer Path</h3>
-                            <p className="text-gray-600">MongoDB</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/adv-node.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://codedamn.com/certificate/verify/a4d85b6c792e80ba979a88527911ca71eb88473f" >
-                            <h3 className="text-lg font-bold">Advanced Node.js Course</h3>
-                            <p className="text-gray-600">codedamn</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/php.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://olympus1.mygreatlearning.com/course_certificate/QWKBPXTC" >
-                            <h3 className="text-lg font-bold">PHP</h3>
-                            <p className="text-gray-600">Great Learning</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/php-adv.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://www.udemy.com/certificate/UC-bea53e2d-7718-4aeb-8881-7cc3bc4e44fc/" >
-                            <h3 className="text-lg font-bold">PHP - Advanced Features</h3>
-                            <p className="text-gray-600">Udemy</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/docker.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://olympus1.mygreatlearning.com/course_certificate/XWLNIRWF" >
-                            <h3 className="text-lg font-bold">Docker for Intermediate Level</h3>
-                            <p className="text-gray-600">Great Learning</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/python.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://www.credly.com/badges/1722d44d-3d8f-474a-a062-fd66d45a27c9/linked_in_profile" >
-                            <h3 className="text-lg font-bold">Python for Data Science</h3>
-                            <p className="text-gray-600">IBM</p>
-                        </Link>
-
-                    </div>
-
-                    <div className="bg-white rounded-lg shadow-md p-4 border-b-4 border-lime-400">
-                        <Image
-                            className="w-full h-40 object-cover rounded-md mb-4"
-                            src="/mysql-cli.png"
-                            width={500}
-                            height={500}
-                            alt='c'
-                        />
-                        <Link href="https://courses.cognitiveclass.ai/certificates/f4f752f200434d90b4cc79f4ff9cd879" >
-                            <h3 className="text-lg font-bold">MySQL command line</h3>
-                            <p className="text-gray-600">Cognitiveclass</p>
-                        </Link>
-
-                    </div>
-
-                </div>
-
-                <p className='text-2xl text-white text-center mt-5 p-4'>
-                    These are a few of my certifications. For a comprehensive list, please visit my <Link href="https://pk.linkedin.com/in/isfhan" className="font-medium text-blue-600 underline hover:text-blue-700 hover:no-underline">LinkedIn profile</Link>  where you can find more details.
-                </p>
+              <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition duration-300 flex flex-col justify-center items-center p-4 rounded-lg">
+                <h3 className="text-lg font-bold text-white text-center">{cert.title}</h3>
+                <p className="text-gray-300">{cert.platform}</p>
+                <Link href={cert.link} target="_blank" rel="noopener noreferrer">
+                  <button className="mt-3 px-4 py-2 text-sm font-semibold text-white bg-lime-500 rounded-md hover:bg-lime-600 transition">
+                    View Certificate
+                  </button>
+                </Link>
+              </div>
             </div>
-
+          ))}
         </div>
-    );
-}
 
+        <p className="text-2xl text-white text-center mt-5 p-4">
+          These are a few of my certifications. For a comprehensive list, please
+          visit my {" "}
+          <Link
+            href="https://pk.linkedin.com/in/isfhan"
+            className="font-medium text-blue-600 underline hover:text-blue-700 hover:no-underline"
+            target="_blank" rel="noopener noreferrer"
+          >
+            LinkedIn profile
+          </Link>{" "}
+          where you can find more details.
+        </p>
+      </div>
+    </div>
+  );
+};
 
+const certifications = [
+  {
+    title: "Web - Internet Fundamentals",
+    platform: "Codedamn",
+    image: "/net-fun.png",
+    link: "https://codedamn.com/certificate/verify/b8be266e289d2659511369640aa385954280c342",
+  },
+  {
+    title: "Web Development Fundamentals",
+    platform: "Sololearn",
+    image: "/wdf.png",
+    link: "https://www.sololearn.com/Certificate/CT-UARFCW4T/pdf",
+  },
+  {
+    title: "Responsive Web Design",
+    platform: "FreeCodeCamp.org",
+    image: "/rwd.png",
+    link: "https://www.freecodecamp.org/certification/isfhan/responsive-web-design",
+  },
+  {
+    title: "Back End Development and APIs",
+    platform: "FreeCodeCamp.org",
+    image: "/bda.png",
+    link: "https://www.freecodecamp.org/certification/isfhan/back-end-development-and-apis",
+  },
+  {
+    title: "NodeJs E-Commerce Web API",
+    platform: "Udemy",
+    image: "/cwa.png",
+    link: "https://www.udemy.com/certificate/UC-b8064000-0225-4f4f-bf6c-4bde61a14bdc/",
+  },
+  {
+    title: "MongoDB Node.js Developer Path",
+    platform: "MongoDB",
+    image: "/mongo-db.png",
+    link: "https://learn.mongodb.com/c/xwdIcLBfQMm0yOXOSIQhXg",
+  },
+  {
+    title: "Advanced Node.js Course",
+    platform: "Codedamn",
+    image: "/adv-node.png",
+    link: "https://codedamn.com/certificate/verify/a4d85b6c792e80ba979a88527911ca71eb88473f",
+  },
+  {
+    title: "PHP",
+    platform: "Great Learning",
+    image: "/php.png",
+    link: "https://olympus1.mygreatlearning.com/course_certificate/QWKBPXTC",
+  },
+  {
+    title: "PHP - Advanced Features",
+    platform: "Udemy",
+    image: "/php-adv.png",
+    link: "https://www.udemy.com/certificate/UC-bea53e2d-7718-4aeb-8881-7cc3bc4e44fc/",
+  },
+  {
+    title: "Docker for Intermediate Level",
+    platform: "Great Learning",
+    image: "/docker.png",
+    link: "https://olympus1.mygreatlearning.com/course_certificate/XWLNIRWF",
+  },
+  {
+    title: "Python for Data Science",
+    platform: "IBM",
+    image: "/python.png",
+    link: "https://www.credly.com/badges/1722d44d-3d8f-474a-a062-fd66d45a27c9/linked_in_profile",
+  },
+  {
+    title: "MySQL command line",
+    platform: "Cognitiveclass",
+    image: "/mysql-cli.png",
+    link: "https://courses.cognitiveclass.ai/certificates/f4f752f200434d90b4cc79f4ff9cd879",
+  },
+];
 
 export default Certifications;
